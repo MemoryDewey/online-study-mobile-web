daa94d8c50da59b79e3994c50649eb9997ac7dff
Dewey <38499618+MemoryDewey@users.noreply.github.com>
2019-09-20 00:16:56 +0800

    添加Passport相关API

--- src/api/passport.js
+++ src/api/passport.js
@@ -4,3 +4,4 @@ export const sendMessage = data => post('/passport/short-message-captcha', data)
 export const login = data => post('/passport/m-login', data);
 export const reset = data => post('/passport/reset', data);
 export const loginByPsw = data => post('/passport/m-login-psw', data);
+export const checkLogin = () => get('/passport/check-login');
