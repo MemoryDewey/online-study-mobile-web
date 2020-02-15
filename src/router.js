1661e52086a94a85da0f4a7eb4529d3b3c3d392f
MemoryDewey <memorydewey@vip.qq.com>
2020-02-15 17:34:58 +0800

    优化router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
 
 Vue.use(Router);
 
@@ -10,7 +9,7 @@ export default new Router({
     routes: [
         {
             path: '/', name: 'home',
-            component: Home,
+            component: () => import('@/views/Home'),
             meta: {title: '首页'}
         },
         {
